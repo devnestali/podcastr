584b170fd6f11aea1290a733e1d97659775608ff
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-06-21 18:37:05 +0200

    fixing import bug

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
-const { format } =  require('date-fns/format');
-import ptBR from 'date-fns/locale/pt-BR';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale/pt-BR';
 
 import styles from './styles.module.scss'
 
 export function Header() {
-  const currentDate = format(new Date(), 'EEEEEE, d MMMM', { locale: ptBR });
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM' , { 
+    locale: ptBR, 
+  });
 
   return(
     <header className={styles.headerContainer}>
