e15580b2f796cca4d5bef63490cc909dbcfb392f
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-06-25 21:38:21 +0200

    Separating episodes data by latest and all

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,22 +4,46 @@ import { ptBR } from 'date-fns/locale';
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
+import styles from './home.module.scss';
+
 type Episode = {
   id: string;
   title: string;
+  thumbnail: string;
+  description: string;
   members: string;
-  published_at: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+
 }
 
 type HomeProps = {
-  episodes: Episode[];
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
 }
 
-export default function Home(props: HomeProps) {
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
   return (
-    <div>
-      <h1>Index</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
+    <div className={styles.homePage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos lançamentos</h2>
+
+        <ul>
+          {latestEpisodes.map(episode => {
+            return (
+              <li>
+                <a href="">{episode.title}</a>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}>
+
+      </section>
     </div>
   )
 }
@@ -39,15 +63,20 @@ export default function Home(props: HomeProps) {
         thumbnail: episode.thumbnail,
         members: episode.members,
         publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+        duration: Number(episode.file.duration),
         durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
         description: episode.description,
         url: episode.file.url,
       };
     })
 
+    const latestEpisodes = episodes.slice(0, 2);
+    const allEpisodes = episodes.slice(2, episodes.length);
+
     return {
       props: {
-        episodes: data,
+        latestEpisodes,
+        allEpisodes
       },
       revalidate: 60 * 60 * 8,
   }
