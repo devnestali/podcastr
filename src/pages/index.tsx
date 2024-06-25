70d2c96309537adb3677645023820aca66be98be
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-06-25 19:44:38 +0200

    Creating types with typescript for index pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,16 @@
-export default function Home(props) {
+import { GetStaticProps } from 'next'
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>Index</h1>
@@ -6,8 +18,8 @@ export default function Home(props) {
     </div>
   )
 }
-  export async function getStaticProps() {
-    const response = await fetch('http://localhost:3333/episodes') // only 3 calls a day
+  export const getStaticProps: GetStaticProps = async () => {
+    const response = await fetch('http://localhost:3333/episodes')
     const data = await response.json()
 
     return {
