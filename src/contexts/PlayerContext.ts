18803a20a17c09ff22b7ef684d8cdf015f4f712a
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-07-04 19:08:02 +0200

    Creating typing for PlayerContext

--- src/contexts/PlayerContext.ts
+++ src/contexts/PlayerContext.ts
@@ -1,4 +1,17 @@
 import { createContext} from 'react';
 
+type Episode = {
+  title: string;
+  members: string;
+  thumbnail: string;
+  duration: string;
+  url: string;
+};
+
+type PlayerContextData = {
+  episodeList: Episode[];
+  currentEpisodeIndex: number;
+};
+
 export const PlayerContext = createContext('');
 
