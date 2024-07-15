5b303c410d1912827ed610cdde20d3e9ecf7145b
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-07-15 18:02:37 +0200

    Handling play and pause using useEffect and useRef by react

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useRef, useEffect } from 'react';
 import { PlayerContext } from '../../contexts/PlayerContext';
 import Slider from 'rc-slider';
 
@@ -7,7 +7,26 @@ import styles from './styles.module.scss'
 import Image from 'next/image';
 
 export function Player() {
-  const { episodeList, currentEpisodeIndex } = useContext(PlayerContext)
+const audioRef = useRef<HTMLAudioElement>(null);
+
+  const { 
+    episodeList, 
+    currentEpisodeIndex, 
+    isPlaying, 
+    togglePlay 
+  } = useContext(PlayerContext)
+
+  useEffect(() => {
+    if(!audioRef.current) {
+      return;
+    }
+
+    if(isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying])
 
   const episode = episodeList[currentEpisodeIndex]
 
@@ -55,6 +74,7 @@ export function Player() {
         { episode && (
           <audio 
             src={episode.url}
+            ref={audioRef}
             autoPlay
           />
         )}
@@ -66,8 +86,16 @@ export function Player() {
           <button type='button' disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
-          <button type='button' className={styles.playButton} disabled={!episode}>
-            <img src="/play.svg" alt="Tocar" />
+          <button 
+          type='button' 
+          className={styles.playButton} 
+          disabled={!episode}
+          onClick={togglePlay}
+          >
+            { isPlaying 
+              ? <img src="/pause.svg" alt="Pausar" />
+              : <img src="/play.svg" alt="Tocar" />
+            }
           </button>
           <button type='button' disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar próxima" />
