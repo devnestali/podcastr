daef451d024c97ba0de10517ec9c0c74510a98aa
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-07-15 16:40:44 +0200

    Doing style for player slider

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,7 +1,8 @@
 import { useContext } from 'react';
-
 import { PlayerContext } from '../../contexts/PlayerContext';
+import Slider from 'rc-slider';
 
+import 'rc-slider/assets/index.css'
 import styles from './styles.module.scss'
 import Image from 'next/image';
 
@@ -34,11 +35,19 @@ export function Player() {
       </div>
       ) }
 
-      <footer className={styles.empty}>
+      <footer className={!episode ? styles.empty : ''}>
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
-            <div className={styles.emptySlider} />  
+            { episode ? (
+              <Slider 
+                trackStyle={{ backgroundColor: '#84d361'}}
+                railStyle={{ backgroundColor: '#9f75ff'}}
+                handleStyle={{ borderColor: '#84d361', borderWidth: 4 }}
+              />
+            ) : (
+              <div className={styles.emptySlider} />
+            )} 
           </div>
           <span>00:00</span>
         </div>
